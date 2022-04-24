92c0712a73f5ad3fe1d593522792e3f8f8cd90bb
Niccari <latieos93@gmail.com>
2022-04-24 11:39:00 +0900

    add: firebase dependency and pipeline yaml

---
+++ Shared/AzurePipelineSPMCachingApp.swift
@@ -0,0 +1,18 @@
+//
+//  AzurePipelineSPMCachingApp.swift
+//  Shared
+//
+//  Created by Kuki Masato on 24/04/2022.
+//
+
+import SwiftUI
+import Firebase
+
+@main
+struct AzurePipelineSPMCachingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Kuki Masato on 24/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Azure Pipeline SPM Caching!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
